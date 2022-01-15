a99fd8b09ea8283783da867943334ff281d01e75
Sylverainv <sylverainv@gmail.com>
2022-01-15 14:53:43 -0500

    file restructuring

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -9,7 +9,34 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id'
+      }
+    }, 
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id'
+      }
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product_tag',
   },
   {
     sequelize,
